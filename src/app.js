fe006cfc1df11c0cf24af622f55b941acb833072
Likhith Sathish <likithlik54@gmail.com>
2025-08-18 19:55:18 +0530

    routing conscept

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express=require('express');
 const app=express();
 
 
-app.use('/node',(req,res)=>{
-res.send('hello there')
-})
+
+// app.get('/node/:id',(req,res)=>{
+// res.send('selected id '+ req.params.id)
+// });  routing  example http://localhost:3000/node/78
+
+
 app.listen(3000, ()=>{
     console.log('sucess')
 })
