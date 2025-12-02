414a534e6170aa2b1869a89529fa3a4afedc1bd8
Likhith Sathish <likithlik54@gmail.com>
2025-12-02 23:58:47 +0530

    added delete API

--- src/app.js
+++ src/app.js
@@ -9,9 +9,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 // POST route - create user
 app.post('/signup', async (req, res) => {
     console.log("Received body:", req.body);
-
     const data = new User(req.body);
-
     try {
         const savedUser = await data.save();
         console.log("Saved user:", savedUser);
@@ -26,17 +24,34 @@ app.get('/user', async (req, res) => {
     const resd = req.body.lastName
     try {
         const ud = await User.find({ lastName: resd })
-        if(ud.length===0){
-            return res.status(404).send('User not found');
-        }else{
+        if (ud.length === 0) {
+            res.status(404).send('User not found');
+        } else {
             console.log("Retrieved user data:", ud);
             res.send(ud)
         }
-        
     }
     catch (err) {
         console.error("Read error:", err);
         res.status(500).send('Error reading user data');
+    }
+})
+
+
+
+app.delete('/user', async (req, res) => {
+    const deluser=req.body.id
+    try {
+        const du=await User.findByIdAndDelete(deluser)
+        if(!du){
+            res.status(404).send('User not found for deletion');
+        }else{
+            console.log("Deleted user data:", du);
+            res.send('User deleted successfully')
+        }
+    } catch (error) {
+        console.error("Delete error:", error);
+        res.status(500).send('Error deleting user data');
     }
 })
 
